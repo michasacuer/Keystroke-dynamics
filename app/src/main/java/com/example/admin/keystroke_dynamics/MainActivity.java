4a9fe38f0d46e3486c704db23e2d5fec2528b5fc
Michał Szewczak <michasacuer3@gmail.com>
2019-01-21 11:08:23 +0100

    Initial commit

---
+++ app/src/main/java/com/example/admin/keystroke_dynamics/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.admin.keystroke_dynamics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
